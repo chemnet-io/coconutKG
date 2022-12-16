65973c87c6a095ab1a8baa7f15e0a20cc9c24207
Stefan Schmidt-Dichte <67461943+ssd-ls-a@users.noreply.github.com>
2022-12-16 14:59:03 +0100

    Update extract_fragments.py

--- extract_fragments.py
+++ extract_fragments.py
@@ -1,40 +1,46 @@
 import json
 from tqdm import tqdm
 
-f = open('cocojson/original/coconut.json')
+f = open('cocojson/original/coconut_02.json')
 
 data = json.load(f)
 fragments_dict = {}
 
+idx = 0
+
 for i in tqdm(data):
+
     for k, v in i.items():
 
         if k == '_id':
+
             compound_id = v['$oid']
 
         if k == 'uniqueNaturalProduct':
-            fragments_dict[compound_id] = {}
-            fragments_dict[compound_id]['id'] = compound_id
-            fragments_dict[compound_id]['fragments'] = []
-            fragments_dict[compound_id]['fragmentsWithSugar'] = []
-            fragments_dict[compound_id]['ertlFunctionalFragments'] = []
-            fragments_dict[compound_id]['ertlFunctionalFragmentsPseudoSmiles'] = []
+            fragments_dict[idx] = {}
+            fragments_dict[idx]['id'] = compound_id
+            fragments_dict[idx]['fragments'] = []
+            fragments_dict[idx]['fragmentsWithSugar'] = []
+            fragments_dict[idx]['ertlFunctionalFragments'] = []
+            fragments_dict[idx]['ertlFunctionalFragmentsPseudoSmiles'] = []
 
             if 'fragments' in v:
                 for frag, frag_int in v['fragments'].items():
-                    fragments_dict[compound_id]['fragments'].append(frag + ':' + str(frag_int))
+                    fragments_dict[idx]['fragments'].append(frag + ':' + str(frag_int))
 
             if 'fragmentsWithSugar' in v:
                 for frag, frag_int in v['fragmentsWithSugar'].items():
-                    fragments_dict[compound_id]['fragmentsWithSugar'].append(frag + ':' + str(frag_int))
+                    fragments_dict[idx]['fragmentsWithSugar'].append(frag + ':' + str(frag_int))
 
             if 'ertlFunctionalFragments' in v:
                 for frag, frag_int in v['ertlFunctionalFragments'].items():
-                    fragments_dict[compound_id]['ertlFunctionalFragments'].append(frag + ':' + str(frag_int))
+                    fragments_dict[idx]['ertlFunctionalFragments'].append(frag + ':' + str(frag_int))
 
             if 'ertlFunctionalFragmentsPseudoSmiles' in v:
                 for frag, frag_int in v['ertlFunctionalFragmentsPseudoSmiles'].items():
-                    fragments_dict[compound_id]['ertlFunctionalFragmentsPseudoSmiles'].append(frag + ':' + str(frag_int))
+                    fragments_dict[idx]['ertlFunctionalFragmentsPseudoSmiles'].append(frag + ':' + str(frag_int))
 
-with open("coconut_fragments.json", "w") as outfile:
+    idx += 1
+
+with open("coconut_fragments_02.json", "w") as outfile:
     json.dump(fragments_dict, outfile)
